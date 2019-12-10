b047d7833149658a804f1550c4ce8993fdcc0dbb
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-12-10 11:32:15 +0100

    smoke testing

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,8 +2,15 @@ pragma solidity ^0.5.11;
 
 contract Election {
     //model candidate
-    uint id;
-    string name;
-    string region;
-    uint voteCount;
+    string public candidate;
+
+
+//Constructor
+constructor () public{
+    candidate = "Candidate 1";
+}
+
+
+
+
 }
